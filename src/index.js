256a62b10807b991e4ca62f37cad0a423a27b7fa
Armando Amezquita <a.amezquita.1009@gmail.com>
2023-03-15 11:14:27 -0500

    draw circle and emit event to different clients

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ app.get('/', (req,res) => {
     res.sendFile(__dirname + '/views/index.html')
 })
 
+const socketsOnline = []; 
+
 // Server of http 
 const httpServer = createServer(app);
 // Server socket
@@ -17,6 +19,11 @@ const io = new Server(httpServer);
 
 // Connection with socket
 io.on('connection', socket => {
+
+    //save socket or clients connected
+    socketsOnline.push(socket.id)
+    console.log('socketsOnline', socketsOnline)
+
     //Clients conect
     console.log('clients conected', io.engine.clientsCount);
     //Id for every client
@@ -25,6 +32,27 @@ io.on('connection', socket => {
     //know if client disconneted
     socket.on('disconnect', () => {
         console.log('The client', socket.id, 'disconnected')
+    })
+
+    //Emit Events to listen in navegator or client 
+    // first parameter = name of event.
+    // second parameter = Content or data that emit to client
+    socket.emit('welcome', 'Now you are connected :)');
+
+    //For emit event for everyone we need to use: io
+    io.emit('nameevent', socket.id + 'user connected');
+
+    //Emit event for the last connected to server
+    socket.on('sayhello', msg => {
+        const lastSocket = socketsOnline[socketsOnline.length - 1]
+        io.to(lastSocket).emit('greet', msg)
+    })
+
+    io.on('connection', socket => {
+        socket.on("circle position", position => {
+            //broadcast => emit events to all users connected but less to me
+            socket.broadcast.emit('move circle', position)
+        })
     })
 
 })
--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,34 +1,87 @@
 // This is the connection with the client. is require in the html document for you can need to use connect
 const socket = io()
 
-function checkSocketStatus(){
-    // return a boolean value
-    console.log('aqui')
-    console.log(socket)
-    console.log('State socket', socket.connected);
-    return socket.connected
+const circle = document.getElementById('circle');
+const drawCircle = (position) => {
+    circle.style.top = position.top;
+    circle.style.left = position.left;
 }
 
+const drag = e => {
+
+    const position = {
+        top: e.clientY + 'px',
+        left: e.clientX + 'px',
+    }
+
+    drawCircle(position)
+    socket.emit('circle position', position)
+}
+
+document.addEventListener("mousedown", e => {
+    document.addEventListener("mousemove", drag)
+})
+
+document.addEventListener("mouseup", e => {
+    document.removeEventListener("mousemove", drag)
+})
+
+socket.on('move circle', position => {
+    drawCircle(position)
+})
+
+// Here recieved event Welcome from Back or server 
+// socket.on('welcome', data => {
+//     console.log('data from backend: ', data)
+//     // Response at client with the data from server
+//     const text = document.querySelector('#text')
+//     text.textContent = data
+// })
+
+// socket.on('nameevent', msg => {
+//     console.log('msg', msg)
+// })
+
+// //Emit event to client from server
+// const emitEventSayHello = document.querySelector('#greeting');
+// emitEventSayHello.addEventListener('click', () => {
+//     console.log('click')
+//     socket.emit('sayhello', "Hello last client connected")
+// })
+
+// socket.on('greet', msg => {
+//     console.log('msg', msg)
+// })
+
+
+// function checkSocketStatus(){
+//     // return a boolean value
+//     console.log('aqui')
+//     console.log(socket)
+//     console.log('State socket', socket.connected);
+//     return socket.connected
+// }
+
 // Client too detected events of socket
-socket.on('connect', () => {
-    console.log('show in client console: conection in client', socket.id)
-    console.log('Socket connected: ', checkSocketStatus())
-})
+// socket.on('connect', () => {
+//     console.log('show in client console: conection in client', socket.id)
+//     console.log('Socket connected: ', checkSocketStatus())    
+// })
 
-// 
-socket.on('disconnect', () => {
-    console.log('The socket was disconnected', socket.id)
-    console.log('Socket disconnected: ', checkSocketStatus())
-})
+// // 
+// socket.on('disconnect', () => {
+//     console.log('The socket was disconnected', socket.id)
+//     console.log('Socket disconnected: ', checkSocketStatus())
+// })
 
-//Event to detect when socket try to connect again
-socket.io.on('reconnect_attempt', () => {
-    console.log('Im try to reconnect')
-})
+// //Event to detect when socket try to connect again
+// socket.io.on('reconnect_attempt', () => {
+//     console.log('Im try to reconnect')
+// })
 
-//Event to listen when socket connected again
-socket.io.on('reconnect', () => {
-    console.log('Im connected again ')
-})
+// //Event to listen when socket connected again
+// socket.io.on('reconnect', () => {
+//     console.log('Im connected again ')
+// })
 
 
